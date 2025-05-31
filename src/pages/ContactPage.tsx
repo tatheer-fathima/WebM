9e3b8c382e52dd31feedfbc0a175aeacf6322fec
T-Fathima <tatheerfathima2004@gmail.com>
2025-05-31 12:51:19 +0530

    small fix

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -72,14 +72,14 @@ const ContactPage: React.FC = () => {
                       <Mail className="w-6 h-6 mr-4 mt-1" />
                       <div>
                         <h3 className="font-semibold mb-1">Email</h3>
-                        <p>info@webm.example.com</p>
+                        <p>tatheerfathima2004@gmail.com</p>
                       </div>
                     </div>
                     <div className="flex items-start">
                       <Phone className="w-6 h-6 mr-4 mt-1" />
                       <div>
                         <h3 className="font-semibold mb-1">Phone</h3>
-                        <p>+1 (555) 123-4567</p>
+                        <p>+91 9398452362</p>
                       </div>
                     </div>
                     <div className="flex items-start">
@@ -87,9 +87,9 @@ const ContactPage: React.FC = () => {
                       <div>
                         <h3 className="font-semibold mb-1">Address</h3>
                         <p>
-                          123 Bookmark Avenue<br />
-                          San Francisco, CA 94103<br />
-                          United States
+                          Visakhapatnam<br />
+                          Andhra Pradesh<br />
+                          India
                         </p>
                       </div>
                     </div>
@@ -97,7 +97,7 @@ const ContactPage: React.FC = () => {
                   <div className="mt-12">
                     <h3 className="font-semibold mb-4">Follow Us</h3>
                     <div className="flex space-x-4">
-                      <a href="#" className="hover:opacity-75 transition-opacity">
+                      <a href="https://www.linkedin.com/in/tatheer-fathima-5ba16b275/" className="hover:opacity-75 transition-opacity">
                         <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
                           <path d="M22.675 0H1.325C.593 0 0 .593 0 1.325v21.351C0 23.407.593 24 1.325 24H12.82v-9.294H9.692v-3.622h3.128V8.413c0-3.1 1.893-4.788 4.659-4.788 1.325 0 2.463.099 2.795.143v3.24l-1.918.001c-1.504 0-1.795.715-1.795 1.763v2.313h3.587l-.467 3.622h-3.12V24h6.116c.73 0 1.323-.593 1.323-1.325V1.325C24 .593 23.407 0 22.675 0z" />
                         </svg>
